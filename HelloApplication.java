cf4e127587aaf116a0cda346e014de28f77eaee6
KarenSandova <sandovalk061@gmail.com>
2024-06-04 23:01:06 -0500

    Create HelloApplication.java

---
+++ HelloApplication.java
@@ -0,0 +1,31 @@
+package com.example.rgb;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number for R: ");
+        int R = scanner.nextInt();
+        System.out.println("Enter a number for G: ");
+        int G = scanner.nextInt();
+        System.out.println("Enter a number for B: ");
+        int B = scanner.nextInt();
+        if ((R >= 40 && R < 80) || (R > 120 && R < 140)) {
+            if ((G >= 0 && G < 30)) {
+                if ((B >= -1 && B < 20)) {
+                }
+            }
+            System.out.println("The result of R is: " + R);
+            System.out.println("The result of G is: " + G);
+            System.out.println("The result of B is: " + B);
+        }
+    }
+}
